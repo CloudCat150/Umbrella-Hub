3e575ac07bcd1d33fe7a3eae4047fa2b45ae4cda
CloudCat150 <cloudcat150@gmail.com>
2024-10-25 03:08:34 +0900

    Your commit message

--- rental/admin.py
+++ rental/admin.py
@@ -3,10 +3,23 @@ from .models import Rental
 
 @admin.action(description='반납으로 표시')
 def mark_as_returned(modeladmin, request, queryset):
-    queryset.delete()
+    queryset.delete()  # 선택한 대여 항목 삭제 (반납으로 표시)
 
 class RentalAdmin(admin.ModelAdmin):
-    list_display = ('name', 'grade', 'class_number', 'student_number')
+    # 관리자가 조회할 수 있는 열을 설정
+    list_display = ('name', 'grade', 'class_number', 'student_number', 'phone_number', 'rented_at', 'return_date')
+    
+    # 정렬 기준
+    ordering = ('-rented_at',)
+
+    # 필터링
+    list_filter = ('grade', 'class_number', 'rented_at')
+    
+    # 검색 기능
+    search_fields = ('name', 'student_number', 'phone_number')
+
+    # 액션 추가
     actions = [mark_as_returned]
 
+# Admin에 모델 등록
 admin.site.register(Rental, RentalAdmin)
--- rental/models.py
+++ rental/models.py
@@ -1,14 +1,18 @@
+# models.py
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 class Rental(models.Model):
     name = models.CharField(max_length=100)
-    grade = models.IntegerField()
-    class_number = models.IntegerField()
-    student_number = models.CharField(max_length=10)
+    class_number = models.CharField(max_length=50)
+    student_number = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15)
+    grade = models.CharField(max_length=10, blank=True, null=True)  # 학년 필드 추가
+    rented_at = models.DateTimeField(auto_now_add=True)
     return_date = models.DateTimeField()
-    rented_at = models.DateTimeField(default=timezone.now)  # 현재 시간으로 기본값 설정
 
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        if not self.return_date:
+            self.return_date = timezone.now() + timedelta(days=5)  # 자동으로 5일 후 날짜 설정
+        super().save(*args, **kwargs)
