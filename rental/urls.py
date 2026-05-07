70fa9179a15f2499ebc78782666bf8631f46a8e3
CloudCat150 <cloudcat150@gmail.com>
2026-05-07 13:57:43 +0900

    urls.py 업데이트

--- rental/urls.py
+++ rental/urls.py
@@ -4,6 +4,5 @@ from .views import create_rental, rental_confirmation, RentalListView, mark_as_r
 urlpatterns = [
     path('rentals/', create_rental, name='create-rental'),  # 대여 등록 페이지
     path('rentals/confirmation/<int:pk>/', rental_confirmation, name='rental_confirmation'),  # 대여 확인 페이지
-    지
     path('rentals/mark_as_returned/<int:rental_id>/', mark_as_returned, name='mark-as-returned'),  # 반납 처리
 ]
