0f926ca2c005ba05b6118e17c6e655e07a818919
CloudCat150 <cloudcat150@gmail.com>
2026-05-07 13:58:51 +0900

    views.py 업데이트

--- rental/views.py
+++ rental/views.py
@@ -24,12 +24,6 @@ def create_rental(request):
 def rental_confirmation(request, pk):
     rental = get_object_or_404(Rental, pk=pk)  # 대여 정보 가져오기
     return render(request, 'rental/rental_confirmation.html', {'rental': rental})  # 확인 템플릿 렌더링
-
-class RentalListView(ListView):
-    model = Rental
-    template_name = 'rental/rental_list.html'  # 사용할 템플릿 파일
-    context_object_name = 'rentals'
-
 def mark_as_returned(request, rental_id):
     rental = get_object_or_404(Rental, id=rental_id)
     rental.delete()  # 대여 항목 삭제 (반납으로 표시)
